4fbb565f0dd41e249f2450ac0fbb2cf379be59bc
dharsiniA <dharsinikce@gmail.com>
2016-10-20 12:04:44 +0530

    Create big.c

---
+++ big.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+int a, b, c;
+printf("Enter a,b,c");
+scanf("%d %d %d",&a,&b,&c);
+if(a>b && a>c){
+printf("a is greatest");}
+else if(b>a && b>c){
+printf("b is greatest");}
+else if(c>a && c>b){
+printf("c is greatest")}
+else{
+printf("all are equal or any two number are equal");
+}
+return 0;
+}
